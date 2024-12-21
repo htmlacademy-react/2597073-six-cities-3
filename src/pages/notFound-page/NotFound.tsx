d00ad200f6c75c51f7deaebe7078fd6db6006041
anmfan <mizev.ivan04@mail.ru>
2024-12-21 22:00:02 +0500

    Маршруты

---
+++ src/pages/notFound-page/NotFound.tsx
@@ -0,0 +1,20 @@
+import {JSX} from 'react';
+import {useNavigate} from 'react-router-dom';
+import style from './style/notfound.module.scss';
+
+function NotFound(): JSX.Element {
+  const nav = useNavigate();
+
+  return (
+    <div className={style.page}>
+      <div className={style.container}>
+        <div className={style.animation}>🤔</div>
+        <h1 className={style.title}>404</h1>
+        <p className={style.text}>Oops! The page you re looking for does not exist.</p>
+        <button className={style.link} onClick={() => nav('/')}>Go Back Home</button>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
